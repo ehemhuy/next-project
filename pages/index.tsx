f5d774ea1a3eb2302f26d1e912311d1eef206dbc
ehemhuy <ehemhuy@gmail.com>
2022-09-27 20:48:40 +0700

    feat: get data from server

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+type Film = {
+  createdDate: string;
+  filmId: string;
+  imgUrl: string;
+  title: string;
+}
+
+type HomeProps = {
+  data: Array<Film>
+}
+
+const Home: NextPage<HomeProps> = ({ data }) => {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +25,15 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {data.map(item => (
+            <a href="#" className={styles.card} key={item.filmId}>
+              <h2>{item.title} &rarr;</h2>
+              <Image height={300} width={300} src={item.imgUrl?.split("?")?.[0]} alt={item.title} />
+              <p>{item.createdDate}</p>
+            </a>
+          ))}
         </div>
       </main>
 
@@ -67,6 +51,11 @@ const Home: NextPage = () => {
       </footer>
     </div>
   )
+}
+export async function getServerSideProps() {
+  const jsonData = await fetch(process.env.NODE_ENV === 'development' ? "http://localhost:3000/films" : "https://nest-project-production.up.railway.app/films")
+  const data: Array<Film> = await jsonData.json();
+  return { props: { data } }
 }
 
 export default Home
