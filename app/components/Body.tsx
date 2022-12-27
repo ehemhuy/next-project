5a5aeefb3ceadb262780e6e59a482ffe92813a6b
ehemhuy <ehemhuy@gmail.com>
2022-12-27 19:41:28 +0700

    feat: Change element tag

--- app/components/Body.tsx
+++ app/components/Body.tsx
@@ -2,8 +2,8 @@ import InfoDetail from './InfoDetail';
 
 export default function Body() {
   return (
-    <div className="py-2 px-[32px] sm:py-2 sm:px-[64px] lg:px-[128px] lg:py-[48px]">
+    <main className="py-2 px-[32px] sm:py-2 sm:px-[64px] lg:px-[128px] lg:py-[48px]">
       <InfoDetail />
-    </div>
+    </main>
   );
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,9 +3,9 @@ import SocialHeader from './SocialHeader';
 
 export default function Header() {
   return (
-    <div className="flex items-center border-b-1  bg-[#8797a8] py-2 px-[32px] sm:py-2 sm:px-[64px] lg:py-6 lg:px-[128px]">
+    <header className="flex items-center border-b-1  bg-[#8797a8] py-2 px-[32px] sm:py-2 sm:px-[64px] lg:py-6 lg:px-[128px]">
       <Logo />
       <SocialHeader />
-    </div>
+    </header>
   );
 }
